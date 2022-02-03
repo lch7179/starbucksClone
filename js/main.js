67733cb99039c37c04ea6d44404b16259faa29da
Dorazi <lch7179@naver.com>
2022-02-03 20:14:14 +0900

    secondCommit

--- js/main.js
+++ js/main.js
@@ -53,9 +53,9 @@ window.addEventListener('scroll', _.throttle(function() {
     spaceBetween: 10,
     centeredSlides: true,
     loop: true,
-    // autoplay: {
-    //   delay: 5000
-    // }
+    autoplay: {
+      delay: 5000
+    },
     pagination: {
       el: '.promotion .swiper-pagination',
       clickable: true
@@ -78,3 +78,22 @@ window.addEventListener('scroll', _.throttle(function() {
       promotionEl.classList.remove('hide');
     }
   });
+
+  function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+    }
+
+    function floatingObject(selector, delay, size) {
+      gsap.to(selector, random(1.5, 2.5), {
+        y: size,
+        repeat: -1,
+        yoyo: true,
+        ease: Power1.easeInOut,
+        delay: random(0, delay)
+      });
+    }
+    floatingObject('.floating1', 1, 15);
+    floatingObject('.floating2', .5, 15);
+    floatingObject('.floating3', 1.5, 20);
