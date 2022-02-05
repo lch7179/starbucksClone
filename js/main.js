3d43f8df672ed1b318cf374c3a73330dbf617086
Dorazi <lch7179@naver.com>
2022-02-05 22:28:16 +0900

    cloneVer

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ searchInputEl.addEventListener('blur', function() {
 });
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function() {
   console.log(window.scrollY);
@@ -24,14 +25,26 @@ window.addEventListener('scroll', _.throttle(function() {
       opacity: 0,
       display: 'none'
     });
+    gsap.to(toTopEl, .2, {
+      x: 0,
+    });
   }
   else{
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
     });
+    gsap.to(toTopEl, .2, {
+      x: 100,
+    });
   }
   }, 300));
+
+  toTopEl.addEventListener('click', function () {
+    gsap.to(window, .7, {
+      scrollTo: 0
+    })
+  });
 
   const fadeEls = document.querySelectorAll('.visual .fade-in');
   fadeEls.forEach(function (fadeEl, index) {
@@ -63,6 +76,16 @@ window.addEventListener('scroll', _.throttle(function() {
     navigation: {
       prevEl: '.promotion .swiper-prev',
       nextEl: '.promotion .swiper-next'
+    }
+  });
+  new Swiper('.awards .swiper-container', {
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+      prevEl:'.awards .swiper-prev',
+      nextEl:'.awards .swiper-next',
     }
   });
 
@@ -97,3 +120,18 @@ window.addEventListener('scroll', _.throttle(function() {
     floatingObject('.floating1', 1, 15);
     floatingObject('.floating2', .5, 15);
     floatingObject('.floating3', 1.5, 20);
+
+    const spyEls = document.querySelectorAll('section.scroll-spy');
+    spyEls.forEach(function (spyEl) {
+      new ScrollMagic
+        .Scene(
+          {
+           triggerElement: spyEl,
+           triggerHook: .8 
+          })
+        .setClassToggle(spyEl,'show')
+        .addTo(new ScrollMagic.Controller());
+    });
+
+    const thisYear = document.querySelector('.this-year');
+    thisYear.textContent = new Date().getFullYear();
